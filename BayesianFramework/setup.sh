8adc85332743b0ee28dc02ccd610726bd36b0e11
Hugh Dickinson <hdickins@umn.edu>
2017-03-22 09:44:01 -0500

    Updated with analysis of latest model grid.

---
+++ BayesianFramework/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export BC03ROOT=/Users/hughdickinson/Documents/Physics/WISPS_Hubble_Constraint/extract_BC03
+export WISPSUMMARYPLOTTER=/Users/hughdickinson/Documents/Development/WISPSummaryPlotter
+export ZEROTHORDERPROCESSING=/Users/hughdickinson/Documents/Development/WISPS-Spectral-Processing/WISPS_Pipeline/
+export PYTHONPATH=${PYTHONPATH}:${BC03ROOT}:${WISPSUMMARYPLOTTER}:${ZEROTHORDERPROCESSING}
